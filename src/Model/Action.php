6143163f23392628e7f731df07d116ea8074bdd2
Kevin Reniers <22906111+kevinreniers@users.noreply.github.com>
2021-03-10 16:39:42 +0100

    add reproducible project

---
+++ src/Model/Action.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Model;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self allow()
+ * @method static self deny()
+ */
+class Action extends Enum
+{
+}
